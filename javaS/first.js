b905b6858a5307ebeb21408cfc969b217590c6e0
dipensiwakoti <depen88siwa@gmail.com>
2024-03-22 13:01:14 +0545

    i have been working in this projeact from past few days, it has been wonderful thrilling journey of mine to use my skills and get to this beautiful conclusion and this is very much pleasant experience for me! Finally today i am adding this to my Github account! Happy to share this here and hope you guys llike this!

---
+++ javaS/first.js
@@ -0,0 +1,105 @@
+
+var textArea = document.querySelector(".addASuggestion textarea");
+var cont = document.querySelector(".page4");
+var full = document.querySelector(".page4 .fullScreen");
+var AllELem = document.querySelectorAll(".page4 .elem");
+var rachana= document.querySelector("#rachana");
+var fotterSendIcon = document.querySelector(".sendSvg-mid-footer");
+var fotterSendIcon1 = document.querySelector(".sendSvg-mid-footer1");
+var hibro = document.querySelector(".sendSvg-mid-footer1");
+var downFotter = document.querySelector(".down-footer");
+var backTop = document.querySelector("#backToTop");
+i=1;
+j=1;
+
+// hibro.addEventListener("mouseenter",function(){
+//   console.log("entered sucesfully", i );
+//   i++;
+// })
+hibro.addEventListener("mouseenter",function(){
+   delete (document.querySelector(".sendSvg-mid-footer"));
+   console.log("entered sucesfully", i );
+   i++;
+   
+   fotterSendIcon1.innerHTML =`<a href="##"> <svg class="sendSvg-mid-footer" fill="#ff7300" height="200px" width="200px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 495.003 495.003" xml:space="preserve" stroke="#ff7300"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="XMLID_51_"> <path id="XMLID_53_" d="M164.711,456.687c0,2.966,1.647,5.686,4.266,7.072c2.617,1.385,5.799,1.207,8.245-0.468l55.09-37.616 l-67.6-32.22V456.687z"></path> <path id="XMLID_52_" d="M492.431,32.443c-1.513-1.395-3.466-2.125-5.44-2.125c-1.19,0-2.377,0.264-3.5,0.816L7.905,264.422 c-4.861,2.389-7.937,7.353-7.904,12.783c0.033,5.423,3.161,10.353,8.057,12.689l125.342,59.724l250.62-205.99L164.455,364.414 l156.145,74.4c1.918,0.919,4.012,1.376,6.084,1.376c1.768,0,3.519-0.322,5.186-0.977c3.637-1.438,6.527-4.318,7.97-7.956 L494.436,41.257C495.66,38.188,494.862,34.679,492.431,32.443z"></path> </g> </g></svg></a>`;
+    // console.log(fotterSendIcon1.innerHTML);
+})
+hibro.addEventListener("mouseleave",function(){
+  delete document.querySelector(".sendSvg-mid-footer");
+  console.log("sucessfully leaved", j );
+  j++;
+  
+  fotterSendIcon1.innerHTML =`<a href="##"> <svg class="sendSvg-mid-footer" fill="#f3f7f5" height="151px" width="151px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-4.95 -4.95 504.90 504.90" xml:space="preserve" transform="rotate(0)matrix(1, 0, 0, 1, 0, 0)" stroke="#f3f7f5"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.9900059999999999"></g><g id="SVGRepo_iconCarrier"> <g id="XMLID_51_"> <path id="XMLID_53_" d="M164.711,456.687c0,2.966,1.647,5.686,4.266,7.072c2.617,1.385,5.799,1.207,8.245-0.468l55.09-37.616 l-67.6-32.22V456.687z"></path> <path id="XMLID_52_" d="M492.431,32.443c-1.513-1.395-3.466-2.125-5.44-2.125c-1.19,0-2.377,0.264-3.5,0.816L7.905,264.422 c-4.861,2.389-7.937,7.353-7.904,12.783c0.033,5.423,3.161,10.353,8.057,12.689l125.342,59.724l250.62-205.99L164.455,364.414 l156.145,74.4c1.918,0.919,4.012,1.376,6.084,1.376c1.768,0,3.519-0.322,5.186-0.977c3.637-1.438,6.527-4.318,7.97-7.956 L494.436,41.257C495.66,38.188,494.862,34.679,492.431,32.443z"></path> </g> </g></svg></a>`;
+  // console.log(fotterSendIcon1.innerHTML);
+})
+
+downFotter.addEventListener("mouseenter",function(){
+  delete document.querySelector(".sendSvg-mid-footer");
+  fotterSendIcon1.innerHTML =`<a href="##"> <svg class="sendSvg-mid-footer" fill="#f3f7f5" height="151px" width="151px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-4.95 -4.95 504.90 504.90" xml:space="preserve" transform="rotate(0)matrix(1, 0, 0, 1, 0, 0)" stroke="#f3f7f5"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.9900059999999999"></g><g id="SVGRepo_iconCarrier"> <g id="XMLID_51_"> <path id="XMLID_53_" d="M164.711,456.687c0,2.966,1.647,5.686,4.266,7.072c2.617,1.385,5.799,1.207,8.245-0.468l55.09-37.616 l-67.6-32.22V456.687z"></path> <path id="XMLID_52_" d="M492.431,32.443c-1.513-1.395-3.466-2.125-5.44-2.125c-1.19,0-2.377,0.264-3.5,0.816L7.905,264.422 c-4.861,2.389-7.937,7.353-7.904,12.783c0.033,5.423,3.161,10.353,8.057,12.689l125.342,59.724l250.62-205.99L164.455,364.414 l156.145,74.4c1.918,0.919,4.012,1.376,6.084,1.376c1.768,0,3.519-0.322,5.186-0.977c3.637-1.438,6.527-4.318,7.97-7.956 L494.436,41.257C495.66,38.188,494.862,34.679,492.431,32.443z"></path> </g> </g></svg></a>`;
+})
+
+var loader= document.querySelector("#loader");
+ setTimeout(function(){
+  loader.style.top= "-100%";
+ },4000)
+cont.addEventListener("mouseenter",function(){
+   full.style.display= "block"
+})
+cont.addEventListener("mouseleave",function(){
+   full.style.display= "none"
+})
+AllELem.forEach(function(each){
+
+  each.addEventListener("mouseenter",function(){
+      console.log(each.childNodes[5].src);
+      
+      full.style.backgroundImage= `url(${each.childNodes[5].src})`; 
+
+    full.addEventListener("mouseenter",function(){
+        full.style.backgroundImage= `url(${each.childNodes[5].src})`;
+        hover.style.top= "0%";
+    })
+
+  } )
+  each.addEventListener("mouseleave",function(){
+
+      full.style.backgroundImage= "none";
+      full.addEventListener("mouseleave",function(){
+        full.style.backgroundImage= "none";
+        hover.style.top= "-100%";
+
+
+    })
+
+  } )
+  full.addEventListener("mouseenter",function (){
+
+  })
+});
+
+backTop.addEventListener("click",function backToTop(){
+  window.scrollTo(0,0);
+});
+
+
+fotterSendIcon1.addEventListener("click",function textAreaInvalid(){
+  var e = document.querySelector(".addASuggestion textarea").value.trim().length;
+  if(e == 0){
+    textArea.placeholder = "Please fill the form !";
+}
+if(e>0){
+  textArea.value = "";
+  textArea.placeholder = "Form sucessfully Submitted!";
+  textArea.style.color = "orangered";
+
+ setTimeout(function(){
+
+   textArea.style.color = "black";
+   textArea.placeholder = "add some sugg....";
+
+
+ },2000)
+}
+});
+
+
---
+++ javaS/responsive.js
@@ -0,0 +1,194 @@
+var menu = document.querySelector("#SVGRepo_iconCarrier");
+var displayMenu = document.querySelector(".menu-display-onclick");
+var svg1 = document.querySelector(".nav-menu-svg1");
+var svg7 = document.querySelector(".nav-menu-svg7");
+var svg = document.querySelector(".nav-menu-svg");
+var wrapper = document.querySelector("#wrapper");
+var video2= document.querySelector(".video2");
+var page2= document.querySelector(".page2");
+var page3= document.querySelector("#page3");
+var page4= document.querySelector(".page4");
+var belowNav = document.querySelector("#bellowNav");
+var logo1 = document.querySelector(".logo1");
+var overlay = document.querySelector(".page4Resposive .overlay");
+var page4Element = document.querySelectorAll(".page4Resposive .elem ");
+var svg3;
+var elementEachPage4;
+var elementEachPage4_2nd;
+var nodeEach;
+var nodeEach_2nd;
+var aA=0;
+var bB=0;
+var cC=0;
+var dD=0;
+var eE=0;
+var zZ;
+
+var flag=0;
+var overlaY=1;
+ belowNav.addEventListener("click",function(){
+   var f = displayMenu.style.top="-100%";
+   if(f=="-100%"){
+    flag=0;
+    svg1.innerHTML=`<svg class="nav-menu-svg" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="none"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill="#000000" fill-rule="evenodd" d="M19 4a1 1 0 01-1 1H2a1 1 0 010-2h16a1 1 0 011 1zm0 6a1 1 0 01-1 1H2a1 1 0 110-2h16a1 1 0 011 1zm-1 7a1 1 0 100-2H2a1 1 0 100 2h16z"></path> </g></svg>
+      <div class="textMenuVal"><p>Menu</p></div>
+      `;
+   }
+
+   else{
+    flag=1;
+    svg1.innerHTML= `<svg class="nav-menu-svg2" fill="#000000" height="200px" width="200px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 490 490" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier2"> <polygon points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 489.292,457.678 277.331,245.004 489.292,32.337 "></polygon> </g></svg>
+    <div class="textMenuVal"><p>Menu</p></div>
+
+    `;
+   }
+  belowNav.style.filter= "blur(0px)";
+  video2.style.filter= "blur(0px)";
+  page2.style.filter= "blur(0px)";
+  page3.style.filter= "blur(0px)";
+  page4.style.filter= "blur(0px)";
+  logo1.style.opacity= "1";
+
+ })
+ page2.addEventListener("click",function(){
+  displayMenu.style.top="-100%";
+  belowNav.style.filter= "blur(0px)";
+  video2.style.filter= "blur(0px)";
+  page2.style.filter= "blur(0px)";
+  page3.style.filter= "blur(0px)";
+  page4.style.filter= "blur(0px)";
+  logo1.style.opacity= "1";
+
+ })
+ video2.addEventListener("click",function(){
+  displayMenu.style.top="-100%";
+  belowNav.style.filter= "blur(0px)";
+  video2.style.filter= "blur(0px)";
+  page2.style.filter= "blur(0px)";
+  page3.style.filter= "blur(0px)";
+  page4.style.filter= "blur(0px)";
+  logo1.style.opacity= "1";
+
+ })
+
+svg7.addEventListener("click",function(){
+    if(flag==0){
+    displayMenu.style.top = "10vh";
+    flag=1;
+    // svg.style.animation = "threeDot .7s ease infinite";
+
+    belowNav.style.filter= "blur(2px)";
+    video2.style.filter= "blur(2px)";
+    page2.style.filter= "blur(2px)";
+    page3.style.filter= "blur(2px)";
+    page4.style.filter= "blur(2px)";
+    logo1.style.opacity= "0";
+    svg.style.display= "none";
+    svg1.innerHTML= `<svg class="nav-menu-svg2" fill="#000000" height="200px" width="200px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 490 490" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier2"> <polygon points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 489.292,457.678 277.331,245.004 489.292,32.337 "></polygon> </g></svg>
+    <div class="textMenuVal"><p>Menu</p></div>
+
+    `;
+    svg3 = document.querySelector(".nav-menu-svg2");
+    svg3.style.display="block";
+  }
+  
+  else{
+    svg3.style.display= "none";
+    displayMenu.style.top = "-100%";
+    flag=0;
+      belowNav.style.filter= "blur(0px)";
+      video2.style.filter= "blur(0px)";
+      page2.style.filter= "blur(0px)";
+      page3.style.filter= "blur(0px)";
+      page4.style.filter= "blur(0px)";
+      logo1.style.opacity= "1";
+      svg.style.display= "block";
+      svg1.innerHTML=`<svg class="nav-menu-svg" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="none"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill="#000000" fill-rule="evenodd" d="M19 4a1 1 0 01-1 1H2a1 1 0 010-2h16a1 1 0 011 1zm0 6a1 1 0 01-1 1H2a1 1 0 110-2h16a1 1 0 011 1zm-1 7a1 1 0 100-2H2a1 1 0 100 2h16z"></path> </g></svg>
+      <div class="textMenuVal"><p>Menu</p></div>
+
+      `;
+    }
+} )
+page4Element.forEach(function(elementEachPage4,nodeEach) {
+   elementEachPage4.addEventListener("click",function(elementEachPage4_2nd,nodeEach_2nd){
+  console.log(elementEachPage4.id);
+  console.log(elementEachPage4.class);
+   zZ= document.querySelector(`.${elementEachPage4.class}.overlay`);
+             if(elementEachPage4.id == 0){
+          zZ.style.top="0%";
+            elementEachPage4.id =1;
+        }
+        else{
+          zZ.top="-100%";
+          elementEachPage4.id = 0;
+
+        }
+      })
+    });
+  // var elementEachPage4.id);
+  // elementEachPage4.forEach(function(elementEachPage4_2nd,nodeEach_2nd){
+       
+  //        console.log(elementEachPage4_2nd);
+
+  // })
+  // console.log(elementEachPage4[0],nodeEach);
+  // elementEachPage4.addEventListener("click",function(){
+
+    
+        // if(){
+        //   overlay.style.top="0%";
+
+        // }
+        // else{
+        //   overlay.top="-100%";
+        // }
+
+  // })
+  
+
+page4Element.addEventListener("click",function(){
+        if(overlaY==1){
+  overlay.style.top="0%";
+overlaY=0;
+}
+
+  else{
+    overlay.style.top ="-100%";
+    overlaY=1;
+  }
+})
+
+// cont.addEventListener("click",function(){
+//   full.style.display= "block"
+// })
+// cont.addEventListener("mouseleave",function(){
+//   full.style.display= "none"
+// })
+// AllELem.forEach(function(each){
+
+//  each.addEventListener("click",function(){
+//      console.log(each.childNodes[5].src);
+     
+//      full.style.backgroundImage= `url(${each.childNodes[5].src})`; 
+
+//    full.addEventListener("click",function(){
+//        full.style.backgroundImage= `url(${each.childNodes[5].src})`;
+//        hover.style.top= "0%";
+//    })
+
+//  } )
+//  each.addEventListener("mouseleave",function(){
+
+//      full.style.backgroundImage= "none";
+//      full.addEventListener("mouseleave",function(){
+//        full.style.backgroundImage= "none";
+//        hover.style.top= "-100%";
+
+
+//    })
+
+//  } )
+//  } )
+
+
+
